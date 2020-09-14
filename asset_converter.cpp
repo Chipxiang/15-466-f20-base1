8e077dc0d2e1ec062d14c67734fed3869830f4c0
wrystal <haowanghh@gmail.com>
2020-09-13 21:51:32 -0400

    split ssize_t

--- asset_converter.cpp
+++ asset_converter.cpp
@@ -6,43 +6,13 @@
 #include "load_save_png.hpp"
 #include "read_write_chunk.hpp"
 #include "data_path.hpp"
+#include "ssize_t.hpp"
 #include <iostream>
 #include <glm/glm.hpp>
 #include <algorithm>
 #include <fstream>
 
-/*
- * from https://stackoverflow.com/questions/34580472/alternative-to-ssize-t-on-posix-unconformant-systems
- * to hack a ssize_t for Windows
- */
-#include <limits.h>
-#include <stddef.h>
-#include <inttypes.h>
-#include <stdint.h>
 
-#if SIZE_MAX == UINT_MAX
-typedef int ssize_t;        /* common 32 bit case */
-#define SSIZE_MIN  INT_MIN
-#define SSIZE_MAX  INT_MAX
-#elif SIZE_MAX == ULONG_MAX
-typedef long ssize_t;       /* linux 64 bits */
-#define SSIZE_MIN  LONG_MIN
-#define SSIZE_MAX  LONG_MAX
-#elif SIZE_MAX == ULLONG_MAX
-typedef long long ssize_t;  /* windows 64 bits */
-#define SSIZE_MIN  LLONG_MIN
-#define SSIZE_MAX  LLONG_MAX
-#elif SIZE_MAX == USHRT_MAX
-typedef short ssize_t;      /* is this even possible? */
-#define SSIZE_MIN  SHRT_MIN
-#define SSIZE_MAX  SHRT_MAX
-#elif SIZE_MAX == UINTMAX_MAX
-typedef uintmax_t ssize_t;  /* last resort, chux suggestion */
-#define SSIZE_MIN  INTMAX_MIN
-#define SSIZE_MAX  INTMAX_MAX
-#else
-#error platform has exotic SIZE_MAX
-#endif
 
 /* Hard code the order of assets we read */
 static const std::vector<std::string> asset_names = {
@@ -53,7 +23,7 @@ static const std::vector<std::string> asset_names = {
     "char_dead",
     "fire",
     "brick",
-    "killer"
+    "killer",
 };
 
 // 8*8 tile in pixel
